f9948e978c21fb280aee990a5fb9fd533a8493c5
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2022-08-09 12:31:20 +0500

    fix: Renamed directories to best practice

--- course-website/docusaurus.config.js
+++ course-website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Full Stack GeoSpatial Development Course',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
